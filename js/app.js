461b1b4b1653bf6c5741ad276068744e8fe51683
xdpiqbx <xdpiqbx@gmail.com>
2021-06-27 02:32:07 -0700

    start prototype

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const subject = "Урок 1. JavaScript. Что такое prototype. JavaScript Prototype";
+const baseUrl = "https://youtu.be/";
+const queryString = "aQkgUUmUJy4?list=PLqKQF2ojwm3l4oPjsB9chrJmlhZ-zOzWT&t=64";
+const url = `${baseUrl}${queryString}`;
+console.log(`=====-> ${subject} <-=====`);
+console.log(`${url}`);
